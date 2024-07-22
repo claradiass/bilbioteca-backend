4a2d00b64987f0fb20f7484ae4d05ad7eef04b36
claradiass <claragoncalves370@gmail.com>
2024-07-22 10:11:00 -0300

    meu primeiro commit

--- Livro.java
+++ Livro.java
@@ -1,3 +1,31 @@
-public class Livro{
-    
+public class Livro implements LivroInterface{
+    String Autor;
+    String Sinopse;
+    String Genero;
+    String Isbn;
+    int Quantidade;
+    String ano;
+
+
+
+    @Override
+    public void add() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+    @Override
+    public void remove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+    @Override
+    public void view() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'view'");
+    }
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
 }
